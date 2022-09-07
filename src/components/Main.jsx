9f21af845d137d1fa918080e6abaca4079fcc943
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-07 12:28:43 +0200

    MODIFIED: changed meme  text position

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import useAPI from './useAPI';
 import Meme from './Meme';
+import './Main.css';
 
 function Main() {
   const [randomIndex, setRandomIndex] = useState(0);
+  const [memeText, setMemeText] = useState({ topText: 'top', bottomText: 'bottom' });
 
   const { memes } = useAPI();
 
@@ -13,9 +15,25 @@ function Main() {
 
   return (
     <div className='main-container'>
-      <h1>Main</h1>
+      <h1>Meme Generator</h1>
+      <form className='form-container'>
+        <label htmlFor="top-text">Enter Top Text</label>
+        <input
+          type="text"
+          id='top-text'
+          name='top-text'
+          value={memeText.topText ?? ''}
+        />
+        <label htmlFor="bottom-text">Enter Bottom Text</label>
+        <input
+          type="text"
+          id='bottom-text'
+          name='bottom-text'
+          value={memeText.bottomText ?? ''}
+        />
+      </form>
       <button className='rnd-button btn' onClick={handleRandomIndex}>Random Picture</button>
-      {memes && <Meme memes={memes} randomIndex={randomIndex} />}
+      {memes && <Meme memes={memes} randomIndex={randomIndex} memeText={memeText} />}
     </div>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,11 @@
-
-function Meme({ memes, randomIndex }) {
+import './Meme.css';
+function Meme({ memes, randomIndex, memeText }) {
 
   return (
     <div className="meme">
-      <img src={memes[randomIndex].url} alt={memes[randomIndex].name} />
+      <h2 className='meme-text top'>{memeText.topText}</h2>
+      {memes.length ? (<img className='meme-picture' src={memes[randomIndex].url} alt={memes[randomIndex].name} />) : ('...loading')}
+      <h2 className='meme-text bottom'>{memeText.bottomText}</h2>)
     </div>
   );
 }
