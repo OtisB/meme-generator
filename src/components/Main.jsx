99c345357ef938f4fb10050c605a57e654381444
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-17 13:53:32 +0200

    ADDED: functunality to upload custom picture

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,7 @@ import './Main.css';
 function Main() {
   const [picIndex, setPicIndex] = useState(0);
   const [memeText, setMemeText] = useState({ topText: '', bottomText: '' });
-
+  const [customPic, setCustomPic] = useState(null);
   const { memes } = useAPI();
 
   const handleNextPicture = () => {
@@ -20,6 +20,14 @@ function Main() {
 
   const handleRandomPicture = () => {
     setPicIndex(Math.floor(Math.random() * memes.length));
+  };
+
+  const handlePictureUpload = (event) => {
+    const { files } = event.target;
+    const { name } = files[0];
+    const url = URL.createObjectURL(files[0]); //static method
+    //console.log(url);
+    setCustomPic({ name, url });
   };
 
   //--------- TEXT LOGIC ----------//
@@ -42,7 +50,14 @@ function Main() {
         <button className='rnd-button' onClick={handleRandomPicture}>Random Picture</button>
         <button className='next-button' onClick={handleNextPicture} disabled={picIndex === memes.length - 1}>Next Picture</button>
       </div>}
-      {memes && <Meme memes={memes} picIndex={picIndex} memeText={memeText} />}
+      {/*acceept works for (IE 10+, Edge (EdgeHTML), Edge (Chromium), Chrome, Firefox 42+) */}
+      <input type="file" name="fileUpload" id="fileUpload" onChange={handlePictureUpload} accept=".jpeg,.jpg, .png" />
+      {memes && <Meme
+        memes={memes}
+        picIndex={picIndex}
+        memeText={memeText}
+        customPic={customPic}
+      />}
     </div>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,14 @@
 import './Meme.css';
-function Meme({ memes, picIndex, memeText }) {
+function Meme({ memes, picIndex, memeText, customPic }) {
 
   return (
     <div className="meme">
       <h2 className='meme-text top'>{memeText.topText}</h2>
-      {memes.length ? (<img className='meme-picture' src={memes[picIndex].url} alt={memes[picIndex].name} />) : ('...loading')}
+      {memes.length ? (<img
+        className='meme-picture'
+        src={customPic ? customPic.url : memes[picIndex].url}
+        alt={customPic ? customPic.name : memes[picIndex].name}
+      />) : ('...loading')}
       <h2 className='meme-text bottom'>{memeText.bottomText}</h2>
     </div>
   );
